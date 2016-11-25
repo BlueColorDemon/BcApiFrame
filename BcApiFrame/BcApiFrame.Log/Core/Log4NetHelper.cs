ff58a0f5fe285bb123e7514841a35b59b024e17b
Administrator <Administrator@AFOJE-703151315>
2016-11-25 10:48:30 +0800

    log配置

---
+++ BcApiFrame/BcApiFrame.Log/Core/Log4NetHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BcApiFrame.Log
+{
+    /// <summary>
+    /// Log4Net 帮助器
+    /// </summary>
+    public class Log4NetHelper
+    {
+        static readonly log4net.ILog loginfo = log4net.LogManager.GetLogger("loginfo");
+        static readonly log4net.ILog logerror = log4net.LogManager.GetLogger("logerror");
+        static readonly log4net.ILog logmonitor = log4net.LogManager.GetLogger("logmonitor");
+
+        public static void Error(string ErrorMsg, Exception ex = null)
+        {
+            if (ex != null)
+            {
+                logerror.Error(ErrorMsg, ex);
+            }
+            else
+            {
+                logerror.Error(ErrorMsg);
+            }
+        }
+
+        public static void Info(string Msg)
+        {
+            loginfo.Info(Msg);
+        }
+
+        public static void Monitor(string Msg)
+        {
+            logmonitor.Info(Msg);
+        }
+    }
+
+    public class Log4NetRegister
+    {
+        public static void Run()
+        {
+            //注册 log4net
+            log4net.Config.XmlConfigurator.Configure(
+                new System.IO.FileInfo(AppDomain.CurrentDomain.BaseDirectory + "\\App_Data\\log4net.config")
+                );
+        }
+    }
+
+}
