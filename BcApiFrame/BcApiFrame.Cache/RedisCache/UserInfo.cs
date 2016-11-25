c1ba6b83bdd988001b6e649b4160eab436a10b49
Administrator <Administrator@AFOJE-703151315>
2016-11-25 17:32:09 +0800

    data

---
+++ BcApiFrame/BcApiFrame.Cache/RedisCache/UserInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BcApiFrame.Cache.RedisCache
+{
+    [Serializable]
+    public class UserInfo
+    {
+        public int Id;
+        public string UserName;
+        public int Age;
+    }
+}
